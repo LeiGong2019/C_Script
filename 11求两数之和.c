10486ce253b9fae8b82f2e3f4d2e77fc4d17085f
LeiGong2019 <1259361157@qq.com>
2022-06-15 01:26:38 +0800

    add 11求两数之和

---
+++ 11求两数之和.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+    int a, b, sum;
+    a = 123;
+    b = 456;
+    sum = a + b;
+    printf("sum is %d \n", sum);
+}
+
+/*
+scanf和printf都是C标准的输入输出函数；
+printf中，变量不要添加"&"符号；
+一个C程序总是从main函数开始执行，无论main函数的位置在哪里；
+每个语句或数据声明，最后一个必须为分号；
+
+*/
